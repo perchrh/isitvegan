7d302a48ba220f34ad5647ca449053fd1abcd8d7
Per Christian Henden <perchrh-gh@pvv.org>
2019-07-29 14:45:44 +0200

    Add menu

--- app/src/main/java/org/digitalsprouts/estoffer/MainActivity.kt
+++ app/src/main/java/org/digitalsprouts/estoffer/MainActivity.kt
@@ -1,7 +1,11 @@
 package org.digitalsprouts.estoffer
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
 import org.digitalsprouts.estoffer.ui.main.SectionsPagerAdapter
@@ -17,5 +21,31 @@ class MainActivity : AppCompatActivity() {
         val tabs: TabLayout = findViewById(R.id.tabs)
         tabs.setupWithViewPager(viewPager)
 
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> {
+                // TODO show settings screen
+                true
+            }
+            R.id.action_about -> {
+                // TODO show about screen
+                true
+            }
+            R.id.action_sources -> {
+                // TODO show sources screen
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
