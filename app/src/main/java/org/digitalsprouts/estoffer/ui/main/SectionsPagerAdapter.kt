df942391220bb8de7831b604742b3410a72cf76b
Per Christian Henden <perchrh-gh@pvv.org>
2019-07-29 12:54:55 +0200

    Add more tabs

--- app/src/main/java/org/digitalsprouts/estoffer/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/org/digitalsprouts/estoffer/ui/main/SectionsPagerAdapter.kt
@@ -8,7 +8,9 @@ import org.digitalsprouts.estoffer.R
 
 private val TAB_TITLES = arrayOf(
     R.string.tab_text_1,
-    R.string.tab_text_2
+    R.string.tab_text_2,
+    R.string.tab_text_3,
+    R.string.tab_text_4
 )
 
 /**
@@ -29,7 +31,6 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
     }
 
     override fun getCount(): Int {
-        // Show 2 total pages.
-        return 2
+        return TAB_TITLES.size
     }
 }
